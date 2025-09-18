f604a6bf7dffdd1e1b6e359ca4f2b97ae960ebf0
Matome Mahlatsi Modiba <geniustoddler@gmail.com>
2025-09-18 15:24:35 +0200

    simple interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,11 @@
+#simple_interest
+
+principal = 1000    
+rate = 0.05         
+time = 3            
+
+# interest
+interest = principal * rate * time
+
+
+print(f"The simple interest is: {interest}")
